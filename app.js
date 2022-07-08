74b77c220247be8acd2f685e98358b56abb474dd
SebastianCarballo <sebastiancarballo.sc.sc@gmail.com>
2022-07-07 21:32:23 -0300

    Termino de crear la estructura del archivo app.js

--- app.js
+++ app.js
@@ -4,3 +4,12 @@ const app = expres();
 const port = 3015;
 
 app.use(expres.static(path.resolve(__dirname, 'public')));
+
+app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, 'views', 'index.html')));
+app.get('/login', (req, res) => res.sendFile(path.resolve(__dirname, 'views', 'login.html')));
+app.get('/register', (req, res) => res.sendFile(path.resolve(__dirname, 'views', 'register.html')));
+
+
+app.listen(port, function() { /* asi se veria un colback con una funcion normal */
+    return console.log(`Se levanto el servidor en http://localhost:${port}`)
+})
