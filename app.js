e69d4d859a617d6c6e226052b8a2eea3c5edc7b4
Sebastian Carballo <sebastiancarballo.sc.sc@gmail.com>
2022-07-15 17:43:29 -0300

    contruyo la estructura para ver los cambios en tiempo real

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
+const livereload = require("livereload");
+const liveReloadServer = livereload.createServer();
+const connectLivereload = require("connect-livereload");
+
+
 const express = require('express');
 const path = require('path');
-const app = expres();
+
+const app = express();
 const port = 3015;
 
-app.use(expres.static(path.resolve(__dirname, 'public')));
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+liveReloadServer.watch(path.join(__dirname,'public'));
+
+app.use(connectLivereload());
+
 
 app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, 'views', 'index.html')));
 app.get('/login', (req, res) => res.sendFile(path.resolve(__dirname, 'views', 'login.html')));
 app.get('/register', (req, res) => res.sendFile(path.resolve(__dirname, 'views', 'register.html')));
+
+liveReloadServer.server.once("connection",() => {
+    setTimeout(() => {
+        liveReloadServer.refresh("/")
+    }, 100);
+});
 
 
 app.listen(port, function() { /* asi se veria un colback con una funcion normal */
